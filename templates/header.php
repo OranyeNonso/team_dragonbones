107284bc7bde91a9c18377e422520653cdab9fe8
Oranye Nonso <nonsooranye@gmail.com>
2019-04-07 00:32:30 +0100

    added Sidebar to header

--- templates/header.php
+++ templates/header.php
@@ -7,10 +7,55 @@
 
     <title>Dragon Stone</title>
 
-    <link rel="stylesheet" href="css/style.css" />
+    <!-- <link rel="stylesheet" href="css/style.css" /> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/sidebarNonix.css">
   </head>
 
   <body>
     <h1>Dragon Stone's App</h1>
+    <head>
+   
+</head>
+<div class="side-bar">
+    <div class="profile">
+        <div class="dp">
+            <img src="css\dp.jpg" alt="Your picture">
+        </div>
+        <div class="about-you">
+           <h3>Oranye Nonso</h3> 
+           <h5>Intern HNG, your favourite C# developer and advocate for STEM education in Nigeria</h5>
+            <h4><a>www.nonix.org</a></h4>
+        </div>
+        <div class="connect-bar">
+            <li><a href="#" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+            <li><a href="#" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+            <li><a href="#" target="_blank"<i class="fab fa-Medium fa-2x"></i></a></li>
+        </div>
+    </div>
+    <div class="saa">
+        <div class="section">
+            <h4 class="stories"><i class="fas fa-file-alt fa-2x"></i> Stories</h4>
+            <p>Latest</p>
+            <p>Web Technology</p>
+            <p>Designs</p>
+            <p>Mobile Development</p>
+            <p>Machine Learning</p>
+        </div>
+        <div class="section">
+           <h4 class="about"><i class="fas fa-user fa-2x"></i> About</h4>
+           <p>Posts</p>
+           <p>Likes</p>
+           <p>Setting</p>
+        </div>
+        <div class="section">
+            <h4 class="address"><i class="far fa-envelope fa-2x"></i> Address</h4>
+            <p>Phones</p>
+            <p>Social Networks</p>
+            <p>Email</p>
+        </div>
+    </div>
+</div>
   </body>
 </html>
